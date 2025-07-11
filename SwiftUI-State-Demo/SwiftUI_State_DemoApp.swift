980d8c237c78a6a3b40539007fd93b76629edec0
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-07-11 14:00:16 -0600

    Initial Commit

---
+++ SwiftUI-State-Demo/SwiftUI_State_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_State_DemoApp.swift
+//  SwiftUI-State-Demo
+//
+//  Created by Benjamin Rojo on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_State_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
